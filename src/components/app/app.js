b73ef20cce0257b8256952da82117ca4e210b3e6
danya <d4nya1v@gmail.com>
2020-11-15 00:46:09 +0300

    button add

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import { Button } from 'reactstrap';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+export default class App extends Component {
 
-export default App;
+    state = {
+        visible: true,
+        nameOpen: 'Открыть',
+        nameClose: 'Скрыть',
+        
+    }
+    onChangeVisible = () => {
+        this.setState(() => {
+            return {
+            visible: !this.state.visible
+        }
+    })
+}
+   
+
+    render(){
+    
+        
+        
+
+       
+        const {visible, nameOpen, nameClose} = this.state;
+        const randomChar = visible ? <RandomChar/> : null
+        const name = randomChar ? nameClose : nameOpen
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+        <Button color="secondary" size="lg" block onClick={this.onChangeVisible}>{name}</Button>
+                            {randomChar}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+}
+
